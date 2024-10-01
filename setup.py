99288650b7a5a15fa00f3e55ba1b176fd7a5b6bb
mikolai.spocinski <spocinskimike@gmail.com>
2024-10-01 11:04:25 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="final_year_proj_1",
+    version="0.1.0",
+    author="Mikolai Spocinski",
+    description="First go at a video to frame exporter and visualiser",
+    packages=find_packages(),
+    install_requires=[
+
+    ]
+)
